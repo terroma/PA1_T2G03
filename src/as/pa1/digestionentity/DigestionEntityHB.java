4e58b23baaf58b45a2dada405a1db9f6dfb11cf7
terroma <terroma@terroma>
2018-03-30 20:37:00 +0100

    Implemented producer of EnrichedTopic_1.

--- src/as/pa1/digestionentity/DigestionEntityHB.java
+++ src/as/pa1/digestionentity/DigestionEntityHB.java
@@ -1,5 +1,6 @@
 package as.pa1.digestionentity;
 
+import as.pa1.data.objets.EnrichedHeartBeat;
 import as.pa1.data.objets.HeartBeat;
 import java.util.Collections;
 import java.util.Properties;
@@ -10,20 +11,27 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 public class DigestionEntityHB {
     
+    private String car_reg = "XX-YY-";
     private final static String ENRICHTOPIC = "EnrichTopic_1";
     private final static String ENRICHEDTOPIC = "EnrichedTopic_1";
+    private final static String CLIENT_ID = "DigestionEntityHB";
     private final static String BOOTSTRAP_SERVERS =
             "localhost:9092, localhost:9093, localhost:9094";
     
     private Consumer<Long, HeartBeat> createConsumer() {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "DigestionEntityHB");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, CLIENT_ID);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "as.pa1.serialization.HeartBeatDeserializer");
 //      props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100);
@@ -32,21 +40,41 @@ public class DigestionEntityHB {
         return consumer;
     }
     
-    
+    private Producer<Long, EnrichedHeartBeat> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, CLIENT_ID);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "as.pa1.serialization.EnrichedHeartBeatSerializer");
+        props.put(ProducerConfig.ACKS_CONFIG,"0");
+        return new KafkaProducer<>(props);
+    }
     
     private void runConsumer() throws InterruptedException {
-        
+        long time = System.currentTimeMillis();
         Consumer<Long, HeartBeat> consumer = createConsumer();
+        Producer<Long, EnrichedHeartBeat> producer = createProducer();
         final int giveUp = 100;
         int noRecordsCount = 0;
+        
         try {
             while (true) {
                 ConsumerRecords<Long, HeartBeat> records = consumer.poll(100);
                 for (ConsumerRecord<Long, HeartBeat> record : records) {
-                    HeartBeat a = record.value();
-                    System.out.println("HeartBeatTime: "+record.value().getTime());
-                    System.out.println("HeartBeatTimeFromA: "+a.getTime());
+                    if (record.value() == null) {
+                        System.out.println("HeartBeat recieved as null.");
+                    } else {
+                        EnrichedHeartBeat enrichedHB = new EnrichedHeartBeat(
+                                record.value().getCar_id(),
+                                record.value().getTime(),
+                                car_reg.concat(String.valueOf(record.value().getCar_id())),
+                                record.value().getMsg_id()
+                        );
+                        producer.send(new ProducerRecord<Long, EnrichedHeartBeat>(ENRICHEDTOPIC,time,enrichedHB)).get();
+                    }
+                    time++;
                 }
+                System.out.println("All records consumed and enriched!!!");
                 /**
                 final ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
 
@@ -70,6 +98,8 @@ public class DigestionEntityHB {
         } finally {
             //consumer.commitSync();
             consumer.close();
+            producer.flush();
+            producer.close();
         }  
     }
     
